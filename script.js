c1b426d507e3bfc7b3b40b55700c33aca9a877e6
joschuld <91635911+joschuld@users.noreply.github.com>
2022-11-28 19:59:37 -0600

    studio Fetch/JSON

--- script.js
+++ script.js
@@ -5,11 +5,13 @@ window.addEventListener('load', function(){
             let dataFetch = data
             console.log(dataFetch) 
         
+        // const div = document.getElementById('container')
+
     for (let i = 0; i < dataFetch.length; i++){
-        
+
         const div = document.getElementById('container');
         
-        div.innerHTML += `
+        div.innerHTML = div.innerHTML + `
             <div class = "astronaut">
                 <div class = "bio">
                     <h3>${dataFetch[i].firstName} ${dataFetch[i].lastName}</h3>
@@ -20,7 +22,6 @@ window.addEventListener('load', function(){
                     </ul>
                 </div>
                 <img class = "avatar" src = ${dataFetch[i].picture}>
-
             </div>
         `;
     
